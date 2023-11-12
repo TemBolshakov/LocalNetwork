ad5612bb2bcc33b6c0633996d01d6b8ced75510e
TemBolshakov <tem_bolshakov@mail.ru>
2023-11-12 13:56:06 +0700

    Start project

---
+++ src/main/java/com/spring/bolshakov/localnetwork/LocalNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.spring.bolshakov.localnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalNetworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocalNetworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/bolshakov/localnetwork/LocalNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.bolshakov.localnetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalNetworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
